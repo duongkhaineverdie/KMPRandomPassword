e7d9be3ee857d9c3bfbddc7dfa02bff48234d0f6
DuongKhai <duongkhaineverdie@gmail.com>
2024-04-16 11:36:14 +0800

    Init project

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -63,7 +63,7 @@ kotlin {
 }
 
 android {
-    namespace = "com.jetbrains.kmpapp"
+    namespace = "com.emenike.randompassword"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
@@ -71,7 +71,7 @@ android {
     sourceSets["main"].resources.srcDirs("src/commonMain/resources")
 
     defaultConfig {
-        applicationId = "com.jetbrains.kmpapp"
+        applicationId = "com.emenike.randompassword"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
